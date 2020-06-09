de663357ad63bf58fff19de31ea8f9246cd9c926
liyiwen <johnnyeven@gmail.com>
2020-06-09 18:38:01 +0800

    mongodb storage implementation

--- internal/modules/m_server.go
+++ internal/modules/m_server.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/gob"
 	"fmt"
+	"longhorn/proxy/internal/constants/enum"
 	"longhorn/proxy/internal/global"
 	"longhorn/proxy/internal/storage"
 )
@@ -17,6 +18,8 @@ type Server struct {
 	Host string `json:"host"`
 	// 端口
 	Port uint16 `json:"port"`
+	// 服务器类型
+	ServerType enum.ServerType `json:"serverType"`
 }
 
 func (v *Server) SetIdentity(id uint64) {
@@ -45,29 +48,104 @@ func (v *Server) GetHost() string {
 	return fmt.Sprintf("%s:%d", v.Host, v.Port)
 }
 
-func CreateServer(c *Server, db storage.Storage) (id uint64, err error) {
-	id, err = db.Create(global.Config.ServerPrefix, c)
-	return
-}
-
 func GetServer(id uint64, db storage.Storage) (c *Server, err error) {
 	err = db.Get(global.Config.ServerPrefix, id, c)
 	return
 }
 
-func WalkServers(start uint64, limit int64, walking func(e storage.Element) error, db storage.Storage) (nextID uint64, err error) {
+type WebServiceServer struct {
+	Server
+}
+
+func (v WebServiceServer) Marshal() (result []byte, err error) {
+	buf := bytes.NewBuffer(result)
+	enc := gob.NewEncoder(buf)
+	err = enc.Encode(v)
+	return buf.Bytes(), err
+}
+
+func (v *WebServiceServer) Unmarshal(data []byte) (err error) {
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	err = dec.Decode(v)
+	return
+}
+
+func CreateWebServiceServer(c *WebServiceServer, db storage.Storage) (id uint64, err error) {
+	id, err = db.Create(global.Config.ServerPrefix, c)
+	return
+}
+
+func GetWebServiceServer(id uint64, db storage.Storage) (c *WebServiceServer, err error) {
+	err = db.Get(global.Config.ServerPrefix, id, c)
+	return
+}
+
+func WalkWebServiceServers(start uint64, limit int64, walking func(e storage.Element) error, db storage.Storage) (nextID uint64, err error) {
 	nextID, err = db.Walk(global.Config.ServerPrefix, start, limit, func() storage.Element {
-		return &Server{}
+		return &WebServiceServer{}
 	}, walking)
 	return
 }
 
-func UpdateServer(c *Server, db storage.Storage) (err error) {
+func UpdateWebServiceServer(c *WebServiceServer, db storage.Storage) (err error) {
 	err = db.Update(global.Config.ServerPrefix, c)
 	return
 }
 
-func DeleteServer(id uint64, db storage.Storage) (err error) {
+func DeleteWebServiceServer(id uint64, db storage.Storage) (err error) {
+	err = db.Delete(global.Config.ServerPrefix, fmt.Sprintf("%d", id))
+	return
+}
+
+type DatabaseServer struct {
+	Server
+
+	// 数据库用户名
+	UserName string `json:"userName"`
+	// 数据库密码
+	Password string `json:"password"`
+	// 数据库配置扩展
+	Extends map[string]string `json:"extends"`
+}
+
+func (v DatabaseServer) Marshal() (result []byte, err error) {
+	buf := bytes.NewBuffer(result)
+	enc := gob.NewEncoder(buf)
+	err = enc.Encode(v)
+	return buf.Bytes(), err
+}
+
+func (v *DatabaseServer) Unmarshal(data []byte) (err error) {
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	err = dec.Decode(v)
+	return
+}
+
+func CreateDatabaseServer(c *DatabaseServer, db storage.Storage) (id uint64, err error) {
+	id, err = db.Create(global.Config.ServerPrefix, c)
+	return
+}
+
+func GetDatabaseServer(id uint64, db storage.Storage) (c *DatabaseServer, err error) {
+	err = db.Get(global.Config.ServerPrefix, id, c)
+	return
+}
+
+func WalkDatabaseServers(start uint64, limit int64, walking func(e storage.Element) error, db storage.Storage) (nextID uint64, err error) {
+	nextID, err = db.Walk(global.Config.ServerPrefix, start, limit, func() storage.Element {
+		return &DatabaseServer{}
+	}, walking)
+	return
+}
+
+func UpdateDatabaseServer(c *DatabaseServer, db storage.Storage) (err error) {
+	err = db.Update(global.Config.ServerPrefix, c)
+	return
+}
+
+func DeleteDatabaseServer(id uint64, db storage.Storage) (err error) {
 	err = db.Delete(global.Config.ServerPrefix, fmt.Sprintf("%d", id))
 	return
 }
